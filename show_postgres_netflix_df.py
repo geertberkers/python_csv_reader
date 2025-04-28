c29883fe35607280095f5a55527d3c5dcaddbbdc
Geert <geertberkers@hotmail.com>
2025-04-29 00:22:22 +0200

    Create show_postgres_netflix_df.py

---
+++ show_postgres_netflix_df.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 28 23:15:19 2025
+
+@author: geert
+"""
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+# Create the engine
+engine = create_engine('postgresql://postgres:password@localhost:5432/netflix_database')
+
+# Read the table into a DataFrame
+df = pd.read_sql('SELECT * FROM netflix;', con=engine)
+
+# Display
+print(df)
