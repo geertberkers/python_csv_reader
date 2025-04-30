a5c2663ff7f20452f247a2a9c02ae9559a122362
Geert <geertberkers@hotmail.com>
2025-04-30 16:04:40 +0200

    Added start of vacatures table

--- script/load_csv_postgres.py
+++ script/load_csv_postgres.py
@@ -28,12 +28,11 @@ print("port:", port)
 print("database:", database)
 
 # 1. Load the CSV
-csv_file = '../csv/development.csv'
+csv_file = '../csv/vacatures.csv'
 #csv_file = '../csv/afschrijvingen.csv'
 df = pd.read_csv(csv_file)
 
 # 2. Define the PostgreSQL connection string
-
 # Format: 'postgresql://username:password@host:port/database'
 connection_string = f'postgresql://{user}:{password}@{host}:{port}/{database}'
 
@@ -41,7 +40,9 @@ connection_string = f'postgresql://{user}:{password}@{host}:{port}/{database}'
 engine = create_engine(connection_string)
 
 # 4. Write the DataFrame into a PostgreSQL table
-df.to_sql('development', con=engine, if_exists='replace', index=False)
+df.to_sql('vacatures', con=engine, if_exists='replace', index=False)
+#df.to_sql('people', con=engine, if_exists='replace', index=False)
+#df.to_sql('development', con=engine, if_exists='replace', index=False)
 #df.to_sql('afschrijvingen', con=engine, if_exists='replace', index=False)
 
 print("CSV data has been loaded into PostgreSQL!")
